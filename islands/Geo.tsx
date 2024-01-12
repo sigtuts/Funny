3e85225375f6c4b4d81ba02fda3b89137b1723e2
Sigve Jordheim <sigve@Sigves-MacBook-Air.local>
2024-01-12 16:04:38 +0100

    Add changes

--- islands/Geo.tsx
+++ islands/Geo.tsx
@@ -3,13 +3,17 @@ import { useState } from "preact/hooks";
 export default function Geo() {
   const [value, setValue] = useState(0);
   const [value2, setValue2] = useState(0);
-  const num23 = parseInt(value) + parseInt(value2);
+  const add = parseFloat(value) + parseFloat(value2);
+  const sub = parseFloat(value) - parseFloat(value2);
+  const mul = parseFloat(value) * parseFloat(value2);
+  const divi = parseFloat(value) / parseFloat(value2);
 
   return (
     <div>
-      <div>Counter: {num23}</div>
-      <div>Counter: {value}</div>
-      <div>Counter: {value2}</div>
+      <div>Add: {add}</div>
+      <div>Subtract: {sub}</div>
+      <div>Multiply: {mul}</div>
+      <div>Divide: {divi}</div>
       <input type="number" onInput={(e) => setValue(e.target.value)} />
       <input type="number" onInput={(e) => setValue2(e.target.value)} />
     </div>
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,27 +1,54 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+// import { useSignal } from "@preact/signals";
+// import Counter from "../islands/Counter.tsx";
 import Geo from "../islands/Geo.tsx";
 
+// export default function Home() {
+//   const count = useSignal(3);
+//   return (
+//     <div class="px-4 py-8 mx-auto bg-[#86efac]">
+//       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+//         <img
+//           class="my-6"
+//           src="/logo.svg"
+//           width="128"
+//           height="128"
+//           alt="the Fresh logo: a sliced lemon dripping with juice"
+//         />
+//         <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+//         <p class="my-4">
+//           Try updating this message in the
+//           <code class="mx-2">./routes/index.tsx</code> file, and refresh. I did
+//         </p>
+//         <Counter count={count} />
+//         <Geo />
+//       </div>
+//     </div>
+//   );
+// }
 export default function Home() {
-  const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh. I did
-        </p>
-        <Counter count={count} />
+    <>
+      <body style="background-color:#232B32">
+        
+      <div style="
+        display: grid;
+        justify-content: space-around;
+        align-content: center;
+        color:#efefef;
+        gap: 4px;
+        grid-auto-flow: column;">
+        <section style="    grid-column: 1;">
+          <img
+            src="/logo.svg"
+            width="128"
+            height="128"
+            alt="the fresh logo: a sliced lemon dripping with juice"
+            />
         <Geo />
+        </section>
       </div>
-    </div>
+      </body>
+
+    </>
   );
 }
